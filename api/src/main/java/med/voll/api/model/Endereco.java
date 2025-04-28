4f71f935680a07337d8910ebfd27c854c5401c70
daniel.quaresma <cipriano.96@hotmail.com>
2025-04-28 17:24:54 -0300

    Iniciando

---
+++ api/src/main/java/med/voll/api/model/Endereco.java
@@ -0,0 +1,6 @@
+package med.voll.api.model;
+
+public record Endereco ( String logradouro, String bairro, String cep, String cidade, String uf, String numero, String complemento){
+
+}
+
---
+++ api/src/main/java/med/voll/api/model/Medico.java
@@ -0,0 +1,7 @@
+package med.voll.api.model;
+
+
+
+public record Medico (String nome, String email,String telefone,String crm, Especialidade especialidade, Endereco endereco){
+
+}
